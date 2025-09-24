14762f6d8a8c82edbea06f49db382f89fd90a0fd
JuhaszBalint-pg <juhasz.balint@diak.szbi-pg.hu>
2025-09-24 08:31:08 +0200

    dolgozat

---
+++ JuhaszBalint_dolgozat/labda.py
@@ -0,0 +1,24 @@
+"""
+A csoport:
+Készítsünk programot, amely labdák csomagolásához végez számításokat.
+A labdákat szalaggal kell átkötni úgy, hogy kétszer körbe érje őket, és a masni készítéséhez számolunk még 60 cm-t.
+A labda körül a szalag egy kör (kerület képlete 2*r*PI)
+A program kérje be a labda átmérőjét, a labdák számát, és a rendelkezésre álló szalag hosszát!
+Számítsa ki, és írja a képernyőre, hogy a bekért számú labda csomagolásához hány centiméter szalagra van szükség, valamint állapítsa meg,
+hogy elegendő szalagunk van-e a művelet elvégzéséhez, és ezt is közölje a felhasználóval!
+"""
+
+print ("csak egész számokat használj!")
+d = int( input ("Add meg a labda átmérőjét cm-ben! " ))
+mennyiseg = int( input ("Add meg mennyi labdát kell becsomagolni! "))
+szalag = int( input ("Add meg a rendelkezére álló szalag hoszzát cm-ben! "))
+masni = 60
+Pi = float(3.14)
+csomagolas = (d*Pi*mennyiseg)*2+masni
+
+print (f"{csomagolas:.3f} hosszúságú szalag kell a csomagoláshoz")
+
+if csomagolas<szalag:
+    print ("Elégséges szalagod van a csomagolás elvégzéséhez")
+else:
+    print (f"Nem elégséges! Valahonna szalagot kell még szerezned!")
